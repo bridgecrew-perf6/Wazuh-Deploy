3a42c8488dd4f640e8e9653491734c95a083c98f
drac0 <29269177+drac0-id@users.noreply.github.com>
2022-06-08 16:27:46 +0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,61 @@
+#drac0
+
+
+apt-get install apt-transport-https zip unzip lsb-release curl gnupg
+curl -s https://artifacts.elastic.co/GPG-KEY-elasticsearch | apt-key add -
+echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | tee /etc/apt/sources.list.d/elastic-7.x.list
+apt-get update
+apt-get install elasticsearch=7.17.3
+curl -so /etc/elasticsearch/elasticsearch.yml https://packages.wazuh.com/4.3/tpl/elastic-basic/elasticsearch_all_in_one.yml
+curl -so /usr/share/elasticsearch/instances.yml https://packages.wazuh.com/4.3/tpl/elastic-basic/instances_aio.yml
+/usr/share/elasticsearch/bin/elasticsearch-certutil cert ca --pem --in instances.yml --keep-ca-key --out ~/certs.zip
+unzip ~/certs.zip -d ~/certs
+mkdir /etc/elasticsearch/certs/ca -p
+cp -R ~/certs/ca/ ~/certs/elasticsearch/* /etc/elasticsearch/certs/
+chown -R elasticsearch: /etc/elasticsearch/certs
+chmod -R 500 /etc/elasticsearch/certs
+chmod 400 /etc/elasticsearch/certs/ca/ca.* /etc/elasticsearch/certs/elasticsearch.*
+rm -rf ~/certs/ ~/certs.zip
+systemctl daemon-reload
+systemctl enable elasticsearch
+systemctl start elasticsearch
+/usr/share/elasticsearch/bin/elasticsearch-setup-passwords auto
+curl -s https://packages.wazuh.com/key/GPG-KEY-WAZUH | apt-key add -
+echo "deb https://packages.wazuh.com/4.x/apt/ stable main" | tee -a /etc/apt/sources.list.d/wazuh.list
+apt-get update
+apt-get install wazuh-manager
+systemctl daemon-reload
+systemctl enable wazuh-manager
+systemctl start wazuh-manager
+systemctl status wazuh-manager
+apt-get install filebeat=7.17.3
+curl -so /etc/filebeat/filebeat.yml https://packages.wazuh.com/4.3/tpl/elastic-basic/filebeat_all_in_one.yml
+curl -so /etc/filebeat/wazuh-template.json https://raw.githubusercontent.com/wazuh/wazuh/4.3/extensions/elasticsearch/7.x/wazuh-template.json
+chmod go+r /etc/filebeat/wazuh-template.json
+curl -s https://packages.wazuh.com/4.x/filebeat/wazuh-filebeat-0.2.tar.gz | tar -xvz -C /usr/share/filebeat/module
+cp -r /etc/elasticsearch/certs/ca/ /etc/filebeat/certs/
+cp /etc/elasticsearch/certs/elasticsearch.crt /etc/filebeat/certs/filebeat.crt
+cp /etc/elasticsearch/certs/elasticsearch.key /etc/filebeat/certs/filebeat.key
+systemctl daemon-reload
+systemctl enable filebeat
+systemctl start filebeat
+apt-get install kibana=7.17.3
+mkdir /etc/kibana/certs/ca -p
+cp -R /etc/elasticsearch/certs/ca/ /etc/kibana/certs/
+cp /etc/elasticsearch/certs/elasticsearch.key /etc/kibana/certs/kibana.key
+cp /etc/elasticsearch/certs/elasticsearch.crt /etc/kibana/certs/kibana.crt
+chown -R kibana:kibana /etc/kibana/
+chmod -R 500 /etc/kibana/certs
+chmod 440 /etc/kibana/certs/ca/ca.* /etc/kibana/certs/kibana.*
+curl -so /etc/kibana/kibana.yml https://packages.wazuh.com/4.3/tpl/elastic-basic/kibana_all_in_one.yml
+mkdir /usr/share/kibana/data
+chown -R kibana:kibana /usr/share/kibana
+cd /usr/share/kibana
+sudo -u kibana /usr/share/kibana/bin/kibana-plugin install https://packages.wazuh.com/4.x/ui/kibana/wazuh_kibana-4.3.3_7.17.3-1.zip
+setcap 'cap_net_bind_service=+ep' /usr/share/kibana/node/bin/node
+systemctl daemon-reload
+systemctl enable kibana
+systemctl start kibana
+sed -i "s/^deb/#deb/" /etc/apt/sources.list.d/wazuh.list
+sed -i "s/^deb/#deb/" /etc/apt/sources.list.d/elastic-7.x.list
+apt-get update
